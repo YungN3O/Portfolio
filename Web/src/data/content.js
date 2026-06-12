7d20a9f1afe5df910b18ce1430f651b89101d498
Vasilis <goumasbilly@gmail.com>
2026-06-13 01:19:41 +0300

    add photo*

--- Web/src/data/content.js
+++ Web/src/data/content.js
@@ -47,7 +47,7 @@ export const about = {
   ],
   // TODO: drop your photo in /public (e.g. /public/me.jpg) and set the path here.
   // Leave as '' to show the placeholder block.
-  photo: '/public/me.jpg',
+  photo: '/public/me.jpeg',
 }
 
 // ---- Experience (cards) ----------------------------------------------------
